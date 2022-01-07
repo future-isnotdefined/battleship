633306e93309267c104c1e678f4c51d8fe985675
unknown <24savage@gmx.de>
2022-01-07 15:59:16 +0100

    laptop test

--- app.c
+++ app.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main (void) {
-    printf("das ist ein test commit.");
+    printf("das ist ein test commit.\n");
+    printf("das ist ein test commit vom laptop - tim.\n");
     return 0;
 }
