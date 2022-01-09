10566f31eae83f6007dd9afec7a9a6ed4f501eaa
tim laptop <24savage@gmx.de>
2022-01-09 18:18:29 +0100

    cleaned up

--- app_REMOTE_577.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main (void) {
-    printf("das ist ein test commit.\n");
-    printf("das ist ein test commit vom laptop - tim.\n");
-    printf("Hello Planet");
-    printf("hdfdf fehler haha nicht zuerst gepullt\n");
-    return 0;
-}
