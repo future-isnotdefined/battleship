8dc11028b36e7f634d310c7a173745d076863b72
Bjoern <bjoern.lemke@freenet.de>
2022-01-09 17:52:24 +0100

    Added printf(FAAAAAAAAAAAAAAAACK!)

--- app.c
+++ app.c
@@ -4,5 +4,6 @@ int main (void) {
     printf("das ist ein test commit.\n");
     printf("das ist ein test commit vom laptop - tim.\n");
     printf("Hello Planet");
+    printf("FAAAAAAAAAAACK!");
     return 0;
 }
