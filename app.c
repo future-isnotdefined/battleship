62584781c3a4b17a55a6beb595e0cd198942b30e
poly_ <77972479+future-isnotdefined@users.noreply.github.com>
2022-01-09 18:14:48 +0100

    clean

--- app.c
+++ app.c
@@ -1,16 +1,5 @@
 #include <stdio.h>
 
 int main (void) {
-    printf("das ist ein test commit.\n");
-    printf("das ist ein test commit vom laptop - tim.\n");
-    printf("Hello Planet");
-    printf("FAAAAAAAAAAACK!");
-    printf("keno 1 ");
-    printf("keno 2 ");
-
-    printf("hdfdf fehler haha nicht zuerst gepullt\n");
-    printf("test 1337");
-    printf("hdfdf fehler haha nicht zuerst gepullt\n");
-    printf("protest");
     return 0;
 }
